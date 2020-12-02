aaf4b5ff65f002ab3a10a06b7630388627987df8
Alexandre Pelagaggi <pelagaggialex@gmail.com>
2020-12-02 01:13:29 -0500

    Code Optimization replacing forEach Loops

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var properJsonOutput = [], IndexedNodes = [],trees=[];
+var properJsonOutput = [], IndexedNodes = [],Trees=[];
 
 class Node {
     constructor(id){
@@ -8,27 +8,40 @@ class Node {
 }
 
 function OrderNodes(inputs){
-    inputs.forEach(input=>{
-        if(IndexedNodes[input.id]!==undefined && IndexedNodes[input.id].name!==undefined){return;}                  //Skip this entry as it is duplicated
-        IndexedNodes[input.id]= Object.assign(new Node(input.id),input,IndexedNodes[input.id]);                     // Initialize Current Node and assign input {id,name,parent_id}
-        IndexedNodes[input.parent_id] = Object.assign(new Node(input.parent_id), IndexedNodes[input.parent_id],);   // Initialize Parent in case its not initialized
-        if(typeof input.parent_id !== typeof 0){
-            trees.push(IndexedNodes[input.id]);
+    //  Loop one time through the input Array Creating a indexed list of double linked Nodes
+    //  The output of this Function will provide Nodes with pointers to its parents and also pointers to its children
+    for (var x=0; x < inputs.length; x++ ){
+        let input = inputs[x];          //  Looping Variable for better code readability
+    
+        if(IndexedNodes[input.id]!==undefined && IndexedNodes[input.id].name!==undefined){continue;}                //  Skip this entry as it is duplicated
+
+        IndexedNodes[input.id]= Object.assign(new Node(input.id),input,IndexedNodes[input.id]);                     //  Initialize Current Node and assign input {id,name,parent_id}
+        
+        IndexedNodes[input.parent_id] = Object.assign(new Node(input.parent_id), IndexedNodes[input.parent_id],);   //  Initialize Parent in case its not initialized
+        
+        if(typeof input.parent_id !== typeof 0){    //  If non numerical id as parent_id (null or Undefined)
+            Trees.push(IndexedNodes[input.id]);     //  Add to list of Trees as Another Root
         }else{
-            IndexedNodes[input.parent_id].children.push(IndexedNodes[input.id]);
+            IndexedNodes[input.parent_id].children.push(IndexedNodes[input.id]);    //  Not a Root So add this node to its parent List of Children
         }
-    })
+    }
 }
 
 function Print(node){
     let {name,id,parent_id} = node;
     properJsonOutput.push({name,id,parent_id:parent_id||null});
-    node.children.forEach(child=>Print(child));
+    
+    for (var x=0; x < node.children.length; x++ ){        // Considering non-binary Trees 0<= Children number <= n
+        Print(node.children[x]);
+    }
+    //  For Binary Trees the loop would be unnecessary
+    //  Recursion(Right_Child);
+    //  Recursion(Left__Child);
 }
 module.exports = function sortCategoriesForInsert(inputJson){
-    properJsonOutput = [], IndexedNodes =[], trees=[];
+    properJsonOutput = [], IndexedNodes =[], Trees=[];
     OrderNodes(inputJson);
-    trees.forEach(rootItem=>{
+    Trees.forEach(rootItem=>{
         Print(rootItem);
     })
   // Your code happens...
