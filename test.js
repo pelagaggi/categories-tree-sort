28e1d498483cb52301e974215d8725adb146aa78
Alexandre Pelagaggi <pelagaggialex@gmail.com>
2020-12-01 22:35:52 -0500

    added stringification assertion scenario

--- test.js
+++ test.js
@@ -1,46 +1,19 @@
 var assert = require('assert');
+const { stringify } = require('querystring');
 const sortCategoriesForInsert = require('./index');
 
-describe('Sort Categories For Insert', function() {
-  describe('#Example From bitbucket', function() {
-    it('should return the same result as the example', function() {
-        let test = [
-            {
-              "name": "Accessories",
-              "id": 1,
-              "parent_id": 20,
-            },
-            {
-              "name": "Watches",
-              "id": 57,
-              "parent_id": 1
-            },
-            {
-              "name": "Men",
-              "id": 20,
-              "parent_id": null,
-            }
-          ];
-          let result = sortCategoriesForInsert(test);
-          let expected = [
-            {
-              "name": "Men",
-              "id": 20,
-              "parent_id": null
-            },
-            {
-              "name": "Accessories",
-              "id": 1,
-              "parent_id": 20
-            },
-            {
-              "name": "Watches",
-              "id": 57,
-              "parent_id": 1
-            }
-          ]
-
-      assert.deepEqual(expected,result);
+describe('Sort Categories For Insert', function () {
+  describe('#Example From bitbucket', function () {
+    let Test_Input = [{ "name": "Men", "id": 20, "parent_id": null }, { "name": "Accessories", "id": 1, "parent_id": 20 }, { "name": "Watches", "id": 57, "parent_id": 1 }];
+    let Expected_Result = [{ "name": "Men", "id": 20, "parent_id": null }, { "name": "Accessories", "id": 1, "parent_id": 20 }, { "name": "Watches", "id": 57, "parent_id": 1 }];
+    it('should return the same result as the example', function () {
+      let result = sortCategoriesForInsert(Test_Input);
+      assert.deepEqual(Expected_Result, result);
+    });
+    it('Should work on a stringified comparison', function () {
+      let result_Stringified = JSON.stringify(sortCategoriesForInsert(Test_Input));
+      let Expected_Stringified = JSON.stringify(Expected_Result);
+      assert.equal(Expected_Stringified, result_Stringified);
     });
   });
 });
