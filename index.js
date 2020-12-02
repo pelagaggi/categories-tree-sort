b0ad64f2703009614513036bf726a9ba6a2b0ffb
Alexandre Pelagaggi <pelagaggialex@gmail.com>
2020-12-01 23:10:09 -0500

    Fixed Properties Order

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ class Node {
 
 function OrderNodes(inputs){
     inputs.forEach(input=>{
-        orderedNodes[input.id]= Object.assign(new Node(input.id), input,orderedNodes[input.id]);                    // Initialize Current Node and assign input {id,name,parent_id}
-        orderedNodes[input.parent_id] = Object.assign(new Node(input.parent_id), orderedNodes[input.parent_id]);    // Initialize Parent in case its not initialized
-        if(input.parent_id===null){
+        orderedNodes[input.id]= Object.assign(new Node(input.id),{parent_id:null}, input,orderedNodes[input.id]);                    // Initialize Current Node and assign input {id,name,parent_id}
+        orderedNodes[input.parent_id] = Object.assign(new Node(input.parent_id), orderedNodes[input.parent_id],);    // Initialize Parent in case its not initialized
+        if(typeof input.parent_id !== typeof 0){
             roots.push(orderedNodes[input.id]);
         }else{
             orderedNodes[input.parent_id].childreen.push(orderedNodes[input.id]);
